8dbc18220b71c1df2cd97cbbb8a74a656ba8a495
NomanMunir <mrnomanmunir@gmail.com>
2023-02-16 07:21:46 +0400

    ex06 done moving onto the ex07

--- C00/ex06/ft_print_combo2.c
+++ C00/ex06/ft_print_combo2.c
@@ -9,6 +9,7 @@ void ft_print_combo2(void)
 {
     int a;
     int b;
+    int c;
 
     a = 0;
     while (a <= 99)
@@ -16,7 +17,32 @@ void ft_print_combo2(void)
         b = a + 1;
         while (b <= 99)
         {
-            ft_putchar(48);
+            c = b + 1;
+            while (c <= 99)
+            {
+                ft_putchar(48 + a / 10);
+                ft_putchar(48 + a % 10);
+                ft_putchar(' ');
+                ft_putchar(48 + b / 10);
+                ft_putchar(48 + b % 10);
+                ft_putchar(' ');
+                ft_putchar(48 + c / 10);
+                ft_putchar(48 + c % 10);
+                if (a != 96)
+                {
+                    ft_putchar(',');
+                    ft_putchar(' ');
+                }
+                c++;
+            }
+            b++;
         }
+        a++;
     }
+}
+
+int main(void)
+{
+    ft_print_combo2();
+    return (0);
 }
