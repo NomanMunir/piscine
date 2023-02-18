7909aaa0433badcd218ff4d672e9b4b5752126e3
NomanMunir <mrnomanmunir@gmail.com>
2023-02-18 08:50:36 +0400

    C01 ex00 done

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+// #include <stdio.h>
+
+void ft_ft(int *nb)
+{
+	*nb = 42;
+}
+
+// int main(void)
+// {	
+// 	int *nb;
+// 	int n = 2;
+// 	nb = &n;
+// 	ft_ft(nb);
+// 	printf("%i", n);
+// 	return (0);
+// }
